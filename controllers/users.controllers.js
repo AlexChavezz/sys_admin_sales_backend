95894a3dee3636dff09efb8b63be44887d035db6
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-11-16 21:59:11 +0000

    add-new-user functionality-added

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -15,34 +15,46 @@ const getUser = (req, res) => {
 }
 
 const addUser = (req, res) => {
-    // const { userName, password, roleId } = req.body;
-    
-    // -> validar si un usuario ya existe
-    // let userName = 'alexis';
-    // let password = "alexis";
-    // let roleId = "CUALQUIER OTRA COSA"
-    // let roleIdStatus = null;
+    const { userName = "alex", password = "111", roleId = "EMPLOYED" } = req.body;
+    let roleIdStatus = null;
+    roleId === "ADMIN" ? (roleIdStatus = 1) : true;
+    roleId === "EMPLOYED" ? (roleIdStatus = 2) : true;
 
-    roleId === "ADMIN"? roleIdStatus = 1: true ;
-    roleId === "EMPLOYED"? roleIdStatus = 2: true;
-
-    if(roleId)
-    {
-        mysqlConnection.query(`INSERT INTO USERS (user_name, password, id_role) VALUES ('${ userName }', '${ password }', ${ roleIdStatus });`, (error, result) => {
-            if(error)
-            {
+    // -> Validate if one user is already saved
+    if (roleIdStatus) {
+        mysqlConnection.query(`SELECT * FROM USERS WHERE user_name='${userName}';`, (error, result) => {
+            if (error) {
+                console.log(error);
                 res.status(500).json({
                     message: "INTERNAL SERVER ERROR",
                 });
             }
-            console.log(result);
-            res.status(200).json(result);
+            if (result.length > 0) {
+                res.status(200).json({
+                    message: "USER ALREADY EXISTS",
+                });
+            } else {
+                // -> Save user into users table.
+                mysqlConnection.query(`INSERT INTO USERS (user_name, password, id_role) VALUES ('${userName}', '${password}', ${roleIdStatus});`, (error, result) => {
+                    if (error) {
+                        res.status(500).json({
+                            message: "INTERNAL SERVER ERROR",
+                        });
+                    }
+                    console.log(result);
+                    res.status(201).json(result);
+                });
+            }
+        });
+    } else {
+        res.status(400).json({
+            message: "Bad request",
         });
     }
-    res.status(400).json({
-        message:"Bad request"
-    });
-}
+};
+
+
+  
 
 module.exports = {
     getUser,
